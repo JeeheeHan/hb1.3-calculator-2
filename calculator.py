c75dedb217d48efa61323cea4f5d3d2b981e465e
Emily <emilye.seo@gmail.com>
2021-02-25 20:32:42 +0000

    debugged code with sean, added .rstrip(), added continue

--- calculator.py
+++ calculator.py
@@ -14,25 +14,25 @@ from arithmetic import *
 
 while True: 
     user_input = input("What is your equation?(type 'q' to exit) > ")
-    token = user_input.split(' ')
+    token = user_input.rstrip().split(' ')
+    
 
     if token[0] is "q":
         break
     elif len(token) < 2:
         print ("Not enough arguments")
-    #else: 
-        #continue
+        continue
 
     math_symbol = token[0]
     num1 = float(token[1])
 
-    if len(token) < 3:
+    if len(token)  == 2:
         if math_symbol == "*":
             print(square(num1))
         elif math_symbol == "**":
             print(cube(num1))
        
-    elif len(token) < 4:
+    elif len(token) == 3:
         num2 = float(token[2])
         if math_symbol == "+":
             print (add(num1, num2))
@@ -49,10 +49,13 @@ while True:
         elif math_symbol == "+**3":
             print(add_cubes(num1, num2))
 
-    elif len(token) < 5:
-        num3 = float(token[3])
+    elif len(token) == 4:
+        num3 = float(token[2])
         if math_symbol == "+*":
             print(add_multi(num1, num2, num3))
+    else:
+        print("Too many arguments")
+        continue
 
 
 
