7dbc059371336b79a4b56708e47d9526041dd078
Emily <emilye.seo@gmail.com>
2021-02-25 02:10:44 +0000

    Spelling errors fixed

--- arithmetic.py
+++ arithmetic.py
@@ -1,40 +1,32 @@
 """Functions for common math operations."""
 
-
 def add(num1, num2):
     """Return the sum of the two input integers."""
     return num1 + num2
-
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
     return num1 - num2
 
-
 def multiply(num1, num2):
     """Multiply the two inputs together."""
     return num1 * num2
-
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
     return float(num1) / num2
 
-
 def square(num1):
     """Return the square of the input."""
     return num1 * num1
-
 
 def cube(num1):
     """Return the cube of the input."""
     return num1 ** 3
 
-
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
     return num1 ** num2
-
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
@@ -43,8 +35,8 @@ def mod(num1, num2):
 def add_multi(num1, num2, num3):
     """ Return first two and multiply sum with the 3rd input"""
     #functions in a function!
-    return  mulitply(add(num1,num2), num3)
+    return multiply(add(num1,num2), num3)
 
 def add_cubes(num1, num2):
-    """Return cube both numbers and sum them"""
-    return add(cub(num1),cub(num2))
+    """Return sum of cubed numbers """
+    return add(cube(num1),cube(num2))
